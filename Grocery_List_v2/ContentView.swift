7bbc3249bc97462b17bb42e4d4718051d7c67ffe
sarahchow <chow.sarah@hotmail.com>
2023-02-21 22:46:33 -0500

    Initial Commit

---
+++ Grocery_List_v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grocery_List_v2
+//
+//  Created by Sarah Chow on 2023-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Grocery_List_v2/Grocery_List_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_List_v2App.swift
+//  Grocery_List_v2
+//
+//  Created by Sarah Chow on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_List_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
